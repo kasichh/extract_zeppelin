f146de44f19516379ead98ee7001a163c6a1e814
pedro <pjesus192@gmail.com>
2023-10-01 12:06:25 -0300

    corrigindo arquivos

--- python/extrair_arquivos.py
+++ python/extrair_arquivos.py
@@ -6,16 +6,11 @@ from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 import pymysql
 
-
 dados = [
     ['TB_STG_MAPA_SPOOLS', 'Mapa Spools.xlsx'],
     ['TB_STG_MAPA_SUPORTE', 'Mapa Suportes.xlsx'],
     ['TB_STG_MAPA_JUNTAS', 'Mapa Juntas.xlsx']
 ]
-dados1 = [
-    ['TB_STG_MAPA_SPOOLS', 'Mapa Spools.xlsx']
-]
-
 
 
 def Mbox(title, text, style):
@@ -26,7 +21,6 @@ def extrair_arquivos(tabela, arquivo):
     engine = create_engine("mysql+pymysql://zep_palmont:%s@186.202.152.83:3306/zep_palmont" % quote_plus("Pm143625@"))
     df = pd.read_sql(f"select * from {tabela}", engine)
     df.to_excel(f'arquivos/{arquivo}', sheet_name=arquivo.split('.xlsx')[0], index=False)
-    return df
 
 
 if __name__ == '__main__':
@@ -35,11 +29,6 @@ if __name__ == '__main__':
         os.mkdir('arquivos')
 
     for mapa in dados:
-        arquivo = extrair_arquivos(mapa[0], mapa[1])
-        # arquivo.to_excel(f'arquivos/{mapa[1]}', index=False)
+        extrair_arquivos(mapa[0], mapa[1])
 
     Mbox('Extração dos arquivos', 'Extração concluída com exito', 1)
-
-
-
-
